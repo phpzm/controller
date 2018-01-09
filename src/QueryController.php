67672c531ac06f2d9bfae14080443a84e77604ac
William Correa <wilcorrea@gmail.com>
2018-01-09 13:58:37 -0200

    QueryBuilder on QueryController

--- src/QueryController.php
+++ src/QueryController.php
@@ -4,6 +4,7 @@ namespace Simples\Controller;
 
 use Simples\Http\Controller;
 use Simples\Http\Response;
+use Simples\Persistence\QueryBuilder;
 
 /**
  * Class QueryController
@@ -11,6 +12,53 @@ use Simples\Http\Response;
  */
 abstract class QueryController extends Controller
 {
+    /**
+     * @var QueryBuilder
+     */
+    protected $queryBuilder;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(QueryBuilder $queryBuilder)
+    {
+        $this->queryBuilder = $queryBuilder;
+    }
+
+    /**
+     * @param string $source
+     * @param string $key
+     * @return string
+     */
+    protected function notDestroyed(string $source, string $key = '_destroyed_at')
+    {
+        return "({$this->queryBuilder->getDriver()::blank("`{$source}`.`{$key}`")})";
+    }
+
+    /**
+     * @param string $alias
+     * @param string $source
+     * @param string $field
+     * @param string $where
+     * @param string $group
+     * @return string
+     */
+    protected function subQuery($alias, $source, $field, $where = null, $group = null)
+    {
+        $pieces = [];
+        $pieces[] = "(";
+        $pieces[] = "SELECT {$field}";
+        $pieces[] = "FROM `{$source}` AS `{$alias}`";
+        if ($where) {
+            $pieces[] = "WHERE {$where}";
+        }
+        if ($group) {
+            $pieces[] = "GROUP BY {$group}";
+        }
+        $pieces[] = ")";
+        return implode(' ', $pieces);
+    }
+
     /**
      * @param mixed $content (null)
      * @param array $meta
